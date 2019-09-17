c17d3dbe7ac1de99ca48ce8c45580157187bf259
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-17 15:31:07 +0100

    code working with regex and correct loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.lang.module.FindException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -17,32 +16,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         String emails = Files.readString(Paths.get("sample.txt"), Charset.forName("windows-1252"));
-        Pattern p = Pattern.compile(".");
-        Matcher m = p.matcher("@softwire.com");
+        Pattern p = Pattern.compile("@softwire\\.com");
+        Matcher m = p.matcher(emails);
         boolean b = m.matches();
 
         int counter = 0;
 
 
-       for (int i =0; i < emails.length() - 13; i++){
-
-           if (){
-
+       while (m.find()){
+           counter = counter + 1;
 
            }
-
-           /*if (emails.substring(i, i + 13 ).equals("@softwire.com")) {
-               counter = counter + 1;*/
-
-           }
-
-
-
-
-
+        System.out.println(counter);
 
        }
-        System.out.println(counter);
+
     }
 
 
