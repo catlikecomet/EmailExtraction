8f5a8c7b3c77017109e5f62ed122460b197fb55a
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-17 12:29:45 +0100

    attempting to count emails

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException{
+        String emails = Files.readString(Paths.get("sample.txt"), Charset.forName("windows-1252"));
+
+
+       int counter = 0;
+
+
+       for (int i =0; i <= emails.length(); i++){
+           if (emails.substring(i, 13 ).equals("@softwire.com")) {
+               counter = counter + 1;
+
+           }
+           System.out.println(counter);
+
+
+
+
+
+       }
+
+    }
+}
+
