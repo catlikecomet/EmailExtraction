d84fe360a88d797a632518e11933731bafec6902
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-17 14:50:44 +0100

    Trying to understand regex...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,38 @@
 package com.company;
 
 import java.io.IOException;
+import java.lang.module.FindException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 
 
 public class Main {
 
 
+
     public static void main(String[] args) throws IOException{
         String emails = Files.readString(Paths.get("sample.txt"), Charset.forName("windows-1252"));
-
+        Pattern p = Pattern.compile(".");
+        Matcher m = p.matcher("@softwire.com");
+        boolean b = m.matches();
 
         int counter = 0;
 
 
        for (int i =0; i < emails.length() - 13; i++){
 
-           if (emails.substring(i, i + 13 ).equals("@softwire.com")) {
-               counter = counter + 1;
+           if (){
+
+
+           }
+
+           /*if (emails.substring(i, i + 13 ).equals("@softwire.com")) {
+               counter = counter + 1;*/
 
            }
 
@@ -31,5 +44,5 @@ public class Main {
        }
         System.out.println(counter);
     }
-}
+
 
